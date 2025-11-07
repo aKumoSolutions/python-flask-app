ccdd0427968efa21731810ca7863b515a317c005
KrisKhuslen <kris@MacBookPro.hsd1.il.comcast.net>
2025-11-06 19:39:14 -0600

    updated app.py

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
-import time 
-for i in range(100):
-    print(i)
-    time.sleep(1)
+from fastapi import FastAPI
 
-# If output is buffered, when will I get logs? = 100 seconds
+app = FastAPI()
 
-# What would happen if your scripts crashes during the run
-
-# If output is not bufferd, when will I get logs? = after every count
-# What would happen if your scripts crashes during the run = I will know exactly at what number the app is crashing on
-
-# Application - critical, cannot be Interrupted
-# Paralell Executions, Can be interrupted = have output buffered
+@app.get("/")
+async def root():
+    return {"message": "Application is runnint"}
